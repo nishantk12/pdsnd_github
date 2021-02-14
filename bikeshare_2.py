cbe00639baf1a8ae9ae1da4ea4559ecd33d69b06
Nishant Kumar <nishantk12@gmail.com>
2021-02-14 18:48:12 +0000

    Change code to accept month from user

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -21,7 +21,7 @@ def get_filters():
 
 
     # get user input for month (all, january, february, ... , june)
-
+    month = input('Enter the Month to filter by, or "all" to apply no month filter')
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
